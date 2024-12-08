abdab3120691eab49cb7769dfbe26576ad7d1010
Abhiram Shibu <abhiramshibu1998@gmail.com>
2024-12-09 00:42:21 +0530

    Setup: Merge install into setup
    Signed-off-by: Abhiram Shibu <abhiramshibu1998@gmail.com>

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 ## Installation
 1. Clone the repository:
-2. Install the required packages:
-     pip install -r requirements.txt
-3. Run the setup: 
-     python setup.py
-4. Run the bot:
-     python bot.py
+2. Run installer
+    ```bash
+        python install.py
+    ```
--- install.py
+++ install.py
@@ -24,6 +24,22 @@ class Installer:
 
         subprocess.check_call([venv_python, "-m", "pip", "install", "-r", "requirements.txt"])
 
+    def setup(self):
+        config_file = "config.py"
+
+        if os.path.exists(config_file):
+            print("Error: config.py already exists. Skipping..")
+        else:
+            bot_api_key = input("Enter bot api key")
+
+            f = open(config_file,"w")
+            f.write("TOKEN = ")
+            f.write(bot_api_key)
+            f.write("\n")
+            f.close()
+
+            print("Wrote config")
+
     def create_service_file(self):
         user = getpass.getuser()
 
@@ -53,6 +69,7 @@ class Installer:
     def main(self):
         self.create_virtual_env()
         self.install_requirements()
+        self.setup()
         self.create_service_file()
         self.install_service()
         self.print_help()
